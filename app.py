0c76aa18eee2ae78179e3f9b0182816cefeb3fcb
sharla martiza yunani <94608562+caramelala@users.noreply.github.com>
2026-02-27 12:36:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import tensorflow as tf
 import numpy as np
+import json
 import os
 import gdown
 from PIL import Image
 
-# WATERMARK
+# CSS WATERMARK
 st.markdown("""
 <style>
 .watermark {
@@ -19,30 +20,26 @@ st.markdown("""
 }
 </style>
 <div class="watermark">
-© Skripsi CNN – VGG16 | Sharla Martiza Yunani
+©️ Skripsi CNN – VGG16 | Sharla Martiza Yunani
 </div>
 """, unsafe_allow_html=True)
 
-# DOWNLOAD MODEL
-MODEL_PATH = "model_afterAug_FT_logits.h5"
-URL = "https://drive.google.com/uc?id=1v7HhpYopAoBMVfnGSC0FIshu4rDX1pku"
-
+# DOWNLOAD MODEL DARI DRIVE
+MODEL_PATH = "model_with_unknown.h5"
+URL = "https://drive.google.com/uc?id=1DnRyCyEQgPQBEcNx28QkwNcIEhw7eQrS"
 if not os.path.exists(MODEL_PATH):
     with st.spinner("Downloading model..."):
         gdown.download(URL, MODEL_PATH, quiet=False)
-
 # LOAD MODEL
 model = tf.keras.models.load_model(MODEL_PATH)
+# LOAD CLASS INDICES
+with open("class_indices.json") as f:
+    class_indices = json.load(f)
+labels = {v: k for k, v in class_indices.items()}
 
-labels = {
-    0: "Geniotrigona thoracica",
-    1: "Tetragonula laeviceps",
-    2: "Tetragonula testaceitarsis",
-    3: "Tetrigona binghami",
-}
-
-# UI
+# STREAMLIT UI
 st.title("🐝 Bee Classification App")
+
 st.markdown(
     """
     <p style='text-align:center; font-size:16px; color:black;'>
@@ -54,45 +51,19 @@ st.markdown(
 )
 
 uploaded_file = st.file_uploader(
-    "Upload gambar",
+    "Upload gambar lebah",
     type=["jpg", "jpeg", "png"]
 )
 
-# PREDIKSI + ENERGY (LOGITS VERSION)
+# PREPROCESS & PREDICT
 if uploaded_file:
-
     img = Image.open(uploaded_file).convert("RGB")
     st.image(img, caption="Uploaded Image", use_column_width=True)
 
-    # preprocessing
     img = img.resize((224, 224))
     img_array = np.array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
-    # 1. GET LOGITS
-    logits = model.predict(img_array, verbose=0)[0]
-
-    # 2. ENERGY SCORE
-    temperature = 1.0
-    energy = -temperature * np.log(
-        np.sum(np.exp(logits / temperature))
-    )
-
-    # 3. SOFTMAX (DISPLAY SAJA)
-    probs = tf.nn.softmax(logits).numpy()
-
-    idx = int(np.argmax(probs))
-    confidence = float(np.max(probs))
-
-    st.write(f"Confidence: {confidence:.4f}")
-    st.write(f"Energy Score: {energy:.4f}")
-
-    # 4. THRESHOLD
-    ENERGY_THRESHOLD = -3.1
-    CONF_THRESHOLD = 0.85
-    
-    if confidence < CONF_THRESHOLD or energy > ENERGY_THRESHOLD:
-        st.warning("Unknown (Objek di luar lebah)")
-    else:
-        label = labels[idx]
-        st.success(f"Prediksi: {label}")
+    pred = model.predict(img_array)
+    idx = np.argmax(pred)
+    st.success(f"Prediksi: **{labels[idx]}**")
