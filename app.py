d6ac16c4fc4d7ac573ede4f1f6a157e408af387d
sharla martiza yunani <94608562+caramelala@users.noreply.github.com>
2026-02-27 11:40:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ if uploaded_file:
     st.write(f"Energy Score: {energy:.4f}")
 
     # 4. THRESHOLD
-    ENERGY_THRESHOLD = 7.0  
+    ENERGY_THRESHOLD = -5.5  
 
     if energy > ENERGY_THRESHOLD:
         st.warning("Prediksi: **Unknown (Objek di luar lebah)**")
