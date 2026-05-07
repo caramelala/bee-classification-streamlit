94163bdaa818462ba1513fdf01f6f07d041164b2
sharla martiza yunani <94608562+caramelala@users.noreply.github.com>
2026-05-07 22:06:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import json
 import os
 import gdown
 from PIL import Image
-# =========================
+
 # CSS WATERMARK
-# =========================
+
 st.markdown("""
 <style>
 .watermark {
@@ -24,27 +24,27 @@ st.markdown("""
 ©️ Skripsi CNN – VGG16 | Sharla Martiza Yunani
 </div>
 """, unsafe_allow_html=True)
-# =========================
+
 # DOWNLOAD MODEL
-# =========================
+
 MODEL_PATH = "model_afterAug_FT_logits.h5"
 URL = "https://drive.google.com/uc?id=1T7fyazI0JiyJM9yBcdZ2P3dsV2hVB3Re"
 if not os.path.exists(MODEL_PATH):
     with st.spinner("Downloading model..."):
         gdown.download(URL, MODEL_PATH, quiet=False)
-# =========================
+
 # LOAD MODEL
-# =========================
+
 model = tf.keras.models.load_model(MODEL_PATH)
-# =========================
+
 # LOAD LABEL
-# =========================
+
 with open("class_indices.json") as f:
     class_indices = json.load(f)
 labels = {v: k for k, v in class_indices.items()}
-# =========================
+
 # UI
-# =========================
+
 st.title("🐝 Bee Classification App")
 st.markdown(
     """
@@ -59,9 +59,9 @@ uploaded_file = st.file_uploader(
     "Upload gambar lebah",
     type=["jpg", "jpeg", "png"]
 )
-# =========================
+
 # PREDIKSI
-# =========================
+
 if uploaded_file:
     img = Image.open(uploaded_file).convert("RGB")
     st.image(img, caption="Uploaded Image", use_column_width=True)
