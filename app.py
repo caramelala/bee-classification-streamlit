e2d9049151c329b366871927dffbb7239be6c33c
sharla martiza yunani <94608562+caramelala@users.noreply.github.com>
2026-05-07 22:04:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import tensorflow as tf
 import numpy as np
+import json
 import os
 import gdown
 from PIL import Image
-
 # =========================
-# WATERMARK
+# CSS WATERMARK
 # =========================
 st.markdown("""
 <style>
@@ -24,33 +24,24 @@ st.markdown("""
 ©️ Skripsi CNN – VGG16 | Sharla Martiza Yunani
 </div>
 """, unsafe_allow_html=True)
-
 # =========================
 # DOWNLOAD MODEL
 # =========================
-MODEL_PATH = "model_afterAug_FT.h5"
-URL = "https://drive.google.com/uc?id=1I7H0W-BNJEhlnUsdjyltoummMShWvfug"
-
+MODEL_PATH = "model_afterAug_FT_logits.h5"
+URL = "https://drive.google.com/uc?id=1T7fyazI0JiyJM9yBcdZ2P3dsV2hVB3Re"
 if not os.path.exists(MODEL_PATH):
     with st.spinner("Downloading model..."):
         gdown.download(URL, MODEL_PATH, quiet=False)
-
 # =========================
 # LOAD MODEL
 # =========================
 model = tf.keras.models.load_model(MODEL_PATH)
-
 # =========================
-# LABEL
+# LOAD LABEL
 # =========================
-labels = {
-    0: "Geniotrigona thoracica",
-    1: "Tetragonula laeviceps",
-    2: "Tetragonula testaceitarsis",
-    3: "Tetrigona binghami",
-    4: "Unknown"
-}
-
+with open("class_indices.json") as f:
+    class_indices = json.load(f)
+labels = {v: k for k, v in class_indices.items()}
 # =========================
 # UI
 # =========================
@@ -64,46 +55,37 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
-# ⬇️ INI YANG BIKIN BALIK KE TAMPILAN LAMA
-st.write("Upload gambar lebah")
-uploaded_file = st.file_uploader("", type=["jpg", "jpeg", "png"])
-
+uploaded_file = st.file_uploader(
+    "Upload gambar lebah",
+    type=["jpg", "jpeg", "png"]
+)
 # =========================
 # PREDIKSI
 # =========================
 if uploaded_file:
     img = Image.open(uploaded_file).convert("RGB")
     st.image(img, caption="Uploaded Image", use_column_width=True)
-
     # Preprocessing
     img = img.resize((224, 224))
     img_array = np.array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
-
     # Predict logits
     logits = model.predict(img_array, verbose=0)[0]
-
     # Temperature
     T = 1.0
-
     # Energy Score
     energy = -T * np.log(
         np.sum(np.exp(logits / T))
     )
-
     # Softmax
     probs = tf.nn.softmax(logits).numpy()
     idx = int(np.argmax(probs))
     confidence = float(np.max(probs))
-
     st.write(f"Confidence: {confidence:.4f}")
     st.write(f"Energy Score: {energy:.4f}")
-
     # Threshold
-    CONF_THRESHOLD = 0.85
-    ENERGY_THRESHOLD = -12
-
+    CONF_THRESHOLD = 0.95
+    ENERGY_THRESHOLD = -17
     # Decision
     if confidence < CONF_THRESHOLD or energy > ENERGY_THRESHOLD:
         st.warning("Prediksi: **Unknown (Objek di luar lebah)**")
